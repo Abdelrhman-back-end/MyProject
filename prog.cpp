8fe112f0267956297022a36c174a1f9f5fdc84c5
Abdlerhman <abdo612200320@gmail.com>
2024-12-19 20:08:34 +0200

    changing prog.cpp File

--- prog.cpp
+++ prog.cpp
@@ -1,7 +1,57 @@
+#include <iterator>
+#include <array>
+#include <vector>
+#include <deque>
+#include <list>
+#include <algorithm>
+#include <utility>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <string>
 #include <iostream>
+#include <algorithm>
+#include <iomanip>
+#define IOS                  \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define dpp(arr, val) memset(arr, val, sizeof(arr))
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define PQ priority_queue
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define S second
+#define F first
+#define MP make_pair
+#define endl "\n"
 using namespace std;
+ll gcd(ll a, ll b)
+{
+    while (b != 0)
+    {
+        int a1 = a;
+        a = b;
+        b = (a1 % b);
+    }
+    return a;
+}
+ll lcm(ll a, ll b)
+{
+    return a / gcd(a, b) * b;
+}
 int main()
 {
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << endl;
 
-    cout << "Hello world" << endl;
+    return 0;
 }
