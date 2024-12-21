fe37600d0bdc2c69cc5a31a800d796e7b5666578
Abdelrhamn Ayman <abdo612200320@gmail.com>
2024-12-21 13:52:34 +0200

    ego message

--- indx.cpp
+++ indx.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 int min()
 {
-    string s = "Hello, World";
-    cout << s << endl;
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 2 ; i*i <= n ; i++)
+    {
+        while(n % i == 0)
+        {
+            n /= i;
+            v.push_back(i);
+        }
+    }
+
+    if(n != 1)
+    {
+        v.push_back(n);
+    }
+
+   for(auto & vv : v)
+   {
+    cout << vv <<" "; 
+   }
+
+    return 0;
 }
